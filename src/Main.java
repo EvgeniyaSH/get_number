8336ba7d404175e6a61b20a1cbf6b66b71252379
Evgenia <e.sukhino@gmail.com>
2017-02-12 22:42:05 +0300

    exeption works

--- src/Main.java
+++ src/Main.java
@@ -4,26 +4,30 @@
 
 public class Main {
     public static void main(String[] args) {
-
-        String given = "08545758";
-        System.out.println(strtoInt(given));
+        try {
+            String given = "08545f58";
+            System.out.println(strtoInt(given));
+        } catch (MyExept ee) {
+            ee.printStackTrace();
+        }
     }
 
+    public static int strtoInt(String given) throws MyExept{
+        int k = given.length();
+        int n = 0;
+        int ourn = 0;
+        while (k > 0) {
+            int i1 = Character.getNumericValue(given.charAt(k - 1));
+            if (i1 < 0 || i1 > 9 )
+                throw new MyExept("one of the symbols in the line is not a number");
+                else
+            ourn = (int) (Math.pow(10, n)) * i1 + ourn;
 
-
-    public static int strtoInt(String given){
-            int k = given.length();
-            int n = 0;
-            int ourn = 0;
-            while (k > 0) {
-                int i1 = Character.getNumericValue(given.charAt(k - 1));
-                ourn = (int) (Math.pow(10, n)) * i1 + ourn;
-
-                k = k - 1;
-                        n = n + 1;
-            }
-            return ourn;
+            k = k - 1;
+            n = n + 1;
         }
+        return ourn;
+    }
 
 
 }
---
+++ src/MyExept.java
@@ -0,0 +1,8 @@
+/**
+ * Created by evgenia on 12.02.17.
+ */
+public class MyExept extends Exception {
+    public MyExept(String mes) {
+super(mes);
+    }
+}
